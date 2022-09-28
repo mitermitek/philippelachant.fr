f27c8c72ecd7d599f7ad1fd5bb84b0ba67a24b4c
Kevin MITERMITE <40229021+mitermitek@users.noreply.github.com>
2022-09-28 20:21:52 +0200

    Showing errors on login page

--- resources/js/Pages/Login.vue
+++ resources/js/Pages/Login.vue
@@ -5,8 +5,12 @@
         <label for="email">Adresse mail</label>
         <input v-model="form.email" id="email" type="email" placeholder="Adresse mail">
 
+        <p v-if="form.errors.email">{{ form.errors.email }}</p>
+
         <label for="password">Mot de passe</label>
         <input v-model="form.password" id="password" type="password" placeholder="Mot de passe">
+
+        <p v-if="form.errors.password">{{ form.errors.password }}</p>
 
         <button type="submit">Connexion</button>
     </form>
@@ -17,6 +21,9 @@ import {useForm} from '@inertiajs/inertia-vue3';
 
 export default {
     name: "Login",
+    props: {
+        errors: Object
+    },
     data() {
         const form = useForm({
             email: null,
