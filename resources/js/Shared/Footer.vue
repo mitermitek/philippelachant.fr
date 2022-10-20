c0b973d5c62ff2dc03bebc581bfb621b3c836654
Kevin MITERMITE <40229021+mitermitek@users.noreply.github.com>
2022-10-20 12:30:18 +0200

    Add footer

---
+++ resources/js/Shared/Footer.vue
@@ -0,0 +1,5 @@
+<template>
+    <footer class="bg-white px-2 sm:px-4 py-2.5 my-10">
+        <p class="text-center text-sm">Fait avec ❤️ par <a href="https://mitermitek.dev" target="_blank" rel="noopener" class="font-semibold">Kevin MITERMITE</a></p>
+    </footer>
+</template>
--- resources/js/Shared/Layout.vue
+++ resources/js/Shared/Layout.vue
@@ -1,18 +1,20 @@
 <template>
-    <AuthenticatedUserBar v-if="$page.props.auth.user" />
+    <AuthenticatedUserBar v-if="$page.props.auth.user"/>
     <Navbar/>
     <main class="container mx-auto py-5 px-3">
         <slot/>
     </main>
+    <Footer/>
 </template>
 
 <script>
 import Navbar from './Navbar.vue';
 import AuthenticatedUserBar from './AuthenticatedUserBar.vue';
+import Footer from './Footer.vue';
 
 export default {
     name: "Layout",
-    components: {Navbar, AuthenticatedUserBar}
+    components: {Navbar, AuthenticatedUserBar, Footer}
 }
 </script>
 
