5490a93ba4007fcd4e9ee30b37ced0d1d96cc90a
Kevin MITERMITE <40229021+mitermitek@users.noreply.github.com>
2022-11-06 17:54:20 +0100

    Sort by created date the images in the gallery

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -10,7 +10,7 @@ class GalleryController extends Controller
     public function index()
     {
         return Inertia::render('Gallery', [
-            'artworks' => Artwork::with('category')->get()
+            'artworks' => Artwork::with('category')->orderByDesc('created_at')->get()
         ]);
     }
 }
